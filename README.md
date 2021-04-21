5480e5fc702488913da12a255a25139e046e2975
Tom Martin <tmartin14@gmail.com>
2021-04-21 10:08:19 -0400

    Updated script and readme

--- README.md
+++ README.md
@@ -1,1 +1,106 @@
 # splunk-appInspect-helper
+
+This script will help in submitting Splunk Apps and Add-Ons to appInspect and for Splunk Cloud vetting.  This script will log in to Splunkbase to obtain a token that is then used to submit the App/Add-On to appInspect.  It will periodically check to see if the process has completed (once per minute) and when completed will retrieve the AppInspect results, save it into a file in the local directory and then open that file in a browser.   
+
+More information on appInspect can be found on [dev.splunk.com](https://dev.splunk.com/enterprise/docs/releaseapps/appinspect/splunkappinspectapi/runappinspectrequestsapi)
+
+## How this works:
+- To login to Splunkbase the command is: 
+
+```
+curl -X GET -u <splunkbase_id> --url "https://api.splunk.com/2.0/rest/login/splunk"
+```
+
+    which results in the following response:
+```json
+{ 
+    "status_code":200,
+    "status":"success",
+    "msg":"Successfully authenticated user and assigned a token",
+    "data":{
+        "token":"xxx",
+        "user":{
+            "name":"xxx",
+            "email":"xxx@zzz.com",
+            "username":"xxx",
+            "groups":["xxxxx","yyyyy","zzzzz"]
+        }
+    }
+}
+```
+- Submiiting an app to appInspect uses the following command:
+
+```
+curl -X POST \
+     -H "Authorization: bearer <token>" \
+     -H "Cache-Control: no-cache" \
+     -F "app_package=@\"<TA_Filename>\"" \
+     --url "https://appinspect.splunk.com/v1/app/validate"
+```
+    which results in the following response:
+```json
+{
+    "request_id": "<request_id>",
+    "message": "Validation request submitted.",
+    "links": [
+        {
+            "rel": "status",
+            "href": "/v1/app/validate/status/c<request_id>"
+        },
+        {
+            "rel": "report",
+            "href": "/v1/app/report/<request_id>"
+        },
+        {
+            "rel": "package",
+            "href": "/v1/app/package/<request_id>"
+        }
+    ]
+}
+```
+
+- Request the status of a submission:
+
+    Once the App/Add-On has been submitted you can check the status using the status url. 
+```
+curl -s -X GET \
+         -H "Authorization: bearer <token>" \
+         --url "https://appinspect.splunk.com/v1/app/validate/status/<request_id>"
+```
+    which results in the following response:
+```json
+{   
+    "request_id": "<request_id>", 
+    "links": [ 
+        { "href": "/v1/app/validate/status/<request_id>",  
+          "rel": "self" 
+        }, 
+        { "href": "/v1/app/report/<request_id>", 
+          "rel": "report" 
+        } 
+    ], 
+    "status": "PROCESSING" 
+}
+```
+- Request the report for a submission:
+
+    Once the process is complete (`status = SUCCESS|FAILURE|FAILED`) you can retrieve the results using the report url above. 
+```
+curl -s -X GET \
+         -H "Authorization: bearer <token>" \
+         --url "https://appinspect.splunk.com/v1/app/report/<request_id>"
+```
+    The report is in HTML format.
+
+
+## Additional Submissions
+Similarlly you can submit a Splunk App or Add-On for Splunk cloud vetting.  The only difference is the addtion of the `included_tags=cloud` parameter used to submit the request.  The curl for cloud vetting is:
+```
+curl -s -X POST \
+     -H "Authorization: bearer <token>" \
+     -H "Cache-Control: no-cache" \
+     -F "app_package=@\"<TA_Filename>\"" \
+     -F "included_tags=cloud" \
+     --url "https://appinspect.splunk.com/v1/app/validate"
+```
+Checking the status of the submission and retrieving the results are the same as appInspect above.
--- submit_appInspect.sh
+++ submit_appInspect.sh
@@ -9,35 +9,96 @@ if [ $# -eq 0 ]; then
     read -p 'Enter the full path to the filename of the Splunk app to submit: ' APP_FILE_PATH 
   else
     APP_FILE_PATH="$1"
-    echo "$APP_FILE_PATH"
 fi
 
 ### Check if the file exists, if not, exit ###
 if [ ! -f "$APP_FILE_PATH" ]; then
     echo "ERROR:    $APP_FILE_PATH does not exist."
-    echo "   Usage:    ./convert.sh TA_Directory"
+    echo
+    echo "   Usage: ${0} <TA_Directory>"
     echo
     exit 9999 # die with error code 9999
 fi
 
-read -p 'Enter the Splunkbase user id to use: ' SPLUNKBASE_USER
+# Login to Splunkbase & get your token
+read -p ' Enter your Splunkbase login: ' SPLUNKBASE_USER
+
+RESPONSE=`curl -s -X GET -u "$SPLUNKBASE_USER" --url "https://api.splunk.com/2.0/rest/login/splunk" `
+STATUS_CODE=`echo $RESPONSE | jq -r '.status_code'`
+if [ $STATUS_CODE -ne 200 ]; then 
+     echo $RESPONSE | jq -r '.msg' 
+     exit 
+fi
+echo Successfully authenticated...
+TOKEN=`echo $RESPONSE | jq -r '.data.token' `
+
+# ----------------------------------------------------
+#  Utlility Functions
+# ----------------------------------------------------
+# Retrieve the status of the a submission
+# check_status( <token> <request_id>)
+check_status(){
+    RESPONSE=`curl -s -X GET \
+         -H "Authorization: bearer $1" \
+         --url "https://appinspect.splunk.com/v1/app/validate/status/$2" `
+     echo $RESPONSE | jq -r .status 
+     return `echo $RESPONSE | jq -r .status | grep -q 'SUCCESS\|FAILURE\|FAILED' `
+}
+
+# Retrieve report results  for a submission
+# get_results( <token> <request_id> <output_filename>) 
+get_results(){
+     echo Retrieving Report...
+     curl -s -X GET \
+          -H "Authorization: bearer $1" \
+          -H "Cache-Control: no-cache" \
+          -H "Content-Type: text/html" \
+         --url "https://appinspect.splunk.com/v1/app/report/$2" > "$3"
+     open "$3"
+}
 
 
-# 1. Login to Splunkbase & get your token
-echo Lets get your TOKEN from Splunkbase...
-TOKEN=`curl -s -X GET -u "$SPLUNKBASE_USER" --url "https://api.splunk.com/2.0/rest/login/splunk" | jq -r '.data.token' `
-
-# 2.  submit an application for validation
+# submit an application for appInspect
+echo
 echo Submitting AppInspect request...
-REQUEST_ID_VALIDATION=`curl -s -X POST \
+RESPONSE=`curl -s -X POST \
      -H "Authorization: bearer $TOKEN" \
      -H "Cache-Control: no-cache" \
      -F "app_package=@\"$APP_FILE_PATH\"" \
-     --url "https://appinspect.splunk.com/v1/app/validate" | jq -r '.request_id' `
-echo request_id=$REQUEST_ID_VALIDATION
+     --url "https://appinspect.splunk.com/v1/app/validate" `
+echo $RESPONSE
+REQUEST_ID_VALIDATION=`echo "$RESPONSE" | jq -r '.request_id' `
+#echo request_id="$REQUEST_ID_VALIDATION"
+echo
+
+# wait for the report to be ready
+while ! check_status "$TOKEN" "$REQUEST_ID_VALIDATION"; do
+     echo "Waiting for appInspect request to complete  $REQUEST_ID_VALIDATION ... this could take several minutes, checking status every 60 seconds"
+     sleep 60
+done
+
+# get the report results  
+get_results "$TOKEN" "$REQUEST_ID_VALIDATION" "appInspect_results.html") 
+
+echo Done.
+echo
 
 
-# 3.  submit review for Cloud Requirements
+
+
+
+: <<'END'
+echo Retrieving appInspect Report...
+curl -s -X GET \
+     -H "Authorization: bearer $TOKEN" \
+         -H "Cache-Control: no-cache" \
+         -H "Content-Type: text/html" \
+         --url "https://appinspect.splunk.com/v1/app/report/$REQUEST_ID_VALIDATION" > report1_appInspect.html
+echo
+echo
+
+
+# submit review for Cloud Requirements
 echo Submitting Cloud Validation request...
 REQUEST_ID_CLOUD=`curl -s -X POST \
      -H "Authorization: bearer $TOKEN" \
@@ -45,10 +106,28 @@ REQUEST_ID_CLOUD=`curl -s -X POST \
      -F "app_package=@\"$APP_FILE_PATH\"" \
      -F "included_tags=cloud" \
      --url "https://appinspect.splunk.com/v1/app/validate" | jq -r '.request_id' `
-echo request_id=$REQUEST_ID_CLOUD
+echo request_id="$REQUEST_ID_CLOUD"
+echo
 
+# wait for the report to be ready
+while ! check_status "$TOKEN" "$REQUEST_ID_VALIDATION"; do
+echo "Waiting for appInspect request to complete  $REQUEST_ID_VALIDATION ... "
+sleep 15
+done
 
-# 4.  submit review for compatibility with Python3
+# get the report results  
+curl -s -X GET \
+     -H "Authorization: bearer $TOKEN" \
+         -H "Cache-Control: no-cache" \
+         -H "Content-Type: text/html" \
+         --url "https://appinspect.splunk.com/v1/app/report/$REQUEST_ID_CLOUD" > report2_cloud.html
+
+echo
+echo
+
+The python3 check is not needed if you are using all python3 code -- skipping this test
+
+# submit review for compatibility with Python3
 echo Submitting Python3 Validation request...
 REQUEST_ID_PYTHON3=`curl -s -X POST \
      -H "Authorization: bearer $TOKEN" \
@@ -56,55 +135,23 @@ REQUEST_ID_PYTHON3=`curl -s -X POST \
      -F "app_package=@\"$APP_FILE_PATH\"" \
      -F "included_tags=py3_migration" \
      --url "https://appinspect.splunk.com/v1/app/validate" | jq -r '.request_id' `
-echo request_id=$REQUEST_ID_PYTHON3
+echo request_id="$REQUEST_ID_PYTHON3"
+echo 
 
-
-
-# 5.  Retrieve the status of the validation
-check_status(){
-    curl -s -X GET \
-         -H "Authorization: bearer $1" \
-         --url "https://appinspect.splunk.com/v1/app/validate/status/$2" | jq -r .status | grep -q 'SUCCESS\|FAILURE\|FAILED'
-}
-
-while ! check_status $TOKEN $REQUEST_ID_VALIDATION; do
-echo Waiting for app validation request to complete  $REQUEST_ID_VALIDATION ... 
-sleep 15
-done
-
-while ! check_status $TOKEN $REQUEST_ID_CLOUD; do
-echo Waiting for cloud validation request to complete  $REQUEST_ID_CLOUD ... 
-sleep 15
-done
-
+# wait for the report to be ready
 while ! check_status $TOKEN $REQUEST_ID_PYTHON3; do
-echo Waiting for app validation request to complete  $REQUEST_ID_PYTHON3 ... 
+echo "Waiting for app validation request to complete  $REQUEST_ID_PYTHON3 ... "
 sleep 15
 done
 
-# 6.Get the report results   (one for each request ID above)
-echo Retrieving Reports...
-curl -s -X GET \
-     -H "Authorization: bearer $TOKEN" \
-         -H "Cache-Control: no-cache" \
-         -H "Content-Type: text/html" \
-         --url "https://appinspect.splunk.com/v1/app/report/$REQUEST_ID_VALIDATION" > report1_appInspect.html
-
-curl -s -X GET \
-     -H "Authorization: bearer $TOKEN" \
-         -H "Cache-Control: no-cache" \
-         -H "Content-Type: text/html" \
-         --url "https://appinspect.splunk.com/v1/app/report/$REQUEST_ID_CLOUD" > report2_cloud.html
-
+# get the report results  
 curl -s -X GET \
      -H "Authorization: bearer $TOKEN" \
          -H "Cache-Control: no-cache" \
          -H "Content-Type: text/html" \
          --url "https://appinspect.splunk.com/v1/app/report/$REQUEST_ID_PYTHON3" > report3_python3.html
 
+END
+
 echo Done.
-
-
-
-
-
+echo
